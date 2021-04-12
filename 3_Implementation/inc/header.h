8bbb31b3566c22fd60a94a32a15aab1de8516932
AYUSHMAN RANU <singhayushman99@gmail.com>
2021-04-13 00:12:40 +0530

    Changes to code

--- 3_Implementation/inc/header.h
+++ 3_Implementation/inc/header.h
@@ -2,7 +2,13 @@
 
 #ifndef __HEADER_H__
 #define __HEADER_H__
-
+/**
+ * @brief Structure for storing filter parameters
+ * R- Resistance
+ * C- Capacitance
+ * L- Inductance
+ * freq - To store calclulated cutoff Frequency
+ */
 typedef struct filter_param
 {
     int r;
@@ -11,6 +17,11 @@ typedef struct filter_param
     float freq;
 }filter_param;
 
+/**
+ * @brief Structure for storing OpAmp Paramters
+ * R1 and R2 - Resistances required for configuration
+ * gain - To store calculated gain
+ */
 typedef struct opamp_param
 {
     int r1;
@@ -18,18 +29,24 @@ typedef struct opamp_param
     float gain;
 }opamp_param;
 
+/**
+ * @brief Structure for storing 555 Timer parameters
+ * R1,R2 and C - Resistors and Capacitor required for configuration into Astable or Monostable Mode
+ * freq - To store frequency of generated square wave in Astable mode and time period of pulse in Monostable mode
+ */
 typedef struct timer_param
 {
     int r1;
     int r2;
     int c;
+    float freq;
 }timer_param;
 
-typedef struct battery_param
+/*typedef struct battery_param
 {
     float voltage;
     float capacity;
-}battery_param;
+}battery_param;*/
 
 typedef enum error_t
 {
@@ -37,6 +54,14 @@ typedef enum error_t
     ERROR=-1
 }error_t;
 
-error_t filer_calc(filter_param f1,int choice);
+error_t filter_calc(filter_param *f1,int choice);
+
+error_t opamp_calc(opamp_param *o1,int choice);
+
+error_t  timer_calc(timer_param *t1,int choice);
+
+float battery_calc(int c, float i);
+
+float reactance(int freq, float val);
 
 #endif
--- 3_Implementation/src/source.c
+++ 3_Implementation/src/source.c
@@ -2,7 +2,7 @@
 #include "header.h"
 #include<math.h>
 
-error_t filer_calc(filter_param f1,int choice)
+error_t filter_calc(filter_param *f1,int choice)
 {
     if(f1==NULL)
         return ERROR;
@@ -10,24 +10,24 @@ error_t filer_calc(filter_param f1,int choice)
         {
             if(choice==1)
             {
-                f1.freq=1/(2*3.14*f1.r*f1.c);
-                printf("Cutoff Frequency of RC Filter : %.3f\n",f1.freq);
+                f1->freq=1/(2*3.14*f1->r*f1->c);
+                printf("Cutoff Frequency of RC Filter : %.3f\n",f1->freq);
             } 
             else if (choice==2)
             {
-                f1.freq=1/(2*3.14*sqrt(f1.l*f1.c));
-                printf("Cutoff Frequency of LC Filter : %.3f\n",f1.freq);
+                f1->freq=1/(2*3.14*sqrt(f1->l*f1->c));
+                printf("Cutoff Frequency of LC Filter : %.3f\n",f1->freq);
             } 
             else if (choice==3)
             {
-                f1.freq=f1.r/(2*3.14*f1.l);
-                printf("Cutoff Frequency of RL Filter : %.3f\n",f1.freq);
+                f1->freq=f1->r/(2*3.14*f1->l);
+                printf("Cutoff Frequency of RL Filter : %.3f\n",f1->freq);
             }
             return SUCCESS;
         }
 }
 
-error_t opamp_calc(opamp_param o1,int choice)
+error_t opamp_calc(opamp_param *o1,int choice)
 {
     if(o1==NULL)
         return ERROR;
@@ -35,17 +35,50 @@ error_t opamp_calc(opamp_param o1,int choice)
     {
         if(choice==1)
         {
-            o1.gain=(1+(o1.r2/o1.r1));
-            printf("Non Inverting Gain : %.2f\n",o1.gain);
+            o1->gain=(1+(o1->r2/o1->r1));
+            printf("Non Inverting Gain : %.2f\n",o1->gain);
         }
         else
         {
-            o1.gain=(1+(o1.r2/o1.r1));
-            printf("Inverting Gain : -%.2f\n",o1.gain);
+            o1->gain=(1+(o1->r2/o1->r1));
+            printf("Inverting Gain : -%.2f\n",o1->gain);
         }
         return SUCCESS;
     }
 
 }
 
-error_t  timer_calc(timer_param t1,)
+error_t  timer_calc(timer_param *t1,int choice)
+{
+    if(t1==NULL)
+        return ERROR;
+    else
+    {
+        if(choice==1)
+        {
+            t1->freq=1.098*t1->r1*t1->c;
+            printf("Pulse Time : %.3f\n",t1->freq);
+        }
+        else
+        {
+            float duty=(t1->r1+t1->r2)/(t1->r1+2*t1->r2);
+            t1->freq=1/(0.693*t1->c*(t1->r1+t1->r2));
+            printf("Duty Cycle : %.3f\n",duty);
+            printf("Frequency of pulse : %.3f\n",t1->freq);
+        }
+        return SUCCESS;
+    }
+}
+
+float battery_calc(int c, float i)
+{
+    float time=(float)c/i;
+    printf("For a constant discharge current of %f A, the battery will last for %f hours",i,time);
+    return time;
+}
+
+float reactance(int freq, float val)
+{
+    float reac=2*3.14*freq*val;
+    return reac;
+}
